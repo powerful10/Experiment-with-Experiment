a148a1df2d2008d4593aecab97d5e50653b1b553
Oybek Abdullayev <oa8315980@gmail.com>
2025-10-28 11:54:05 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/Experimenting-with-Experiment/',
+  base:'/Experiment-with-Experiment/',
   plugins: [react()],
   resolve: {
     alias: {
